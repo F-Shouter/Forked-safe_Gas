8582f078173f83f21ada55d8e3ee8fac2baaf3a8
F-Shouter <yukari.yamasaki1809@gmail.com>
2025-05-15 22:49:03 -0300

    cadastro function em usuarioModel atualizado

--- Sprint3/Desenvolvimento/Algoritmo/siteInstitucional/src/models/usuarioModel.js
+++ Sprint3/Desenvolvimento/Algoritmo/siteInstitucional/src/models/usuarioModel.js
@@ -16,7 +16,7 @@ function cadastrar(nome, cep, logradouro, numero_log, cnpj, senha, fkEmpresa) {
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucaoSql = `
-        INSERT INTO condominio (nome, cep, logradouro, numero_log, cnpj, senha) VALUES ('${nome}', '${cep}', '${logradouro}', '${numero_log}', '${cnpj}',  '${senha}');
+        INSERT INTO condominio (nome_condominio, cep, logradouro, numero_logradouro, cnpj, senha) VALUES ('${nome}', '${cep}', '${logradouro}', '${numlog}', '${cnpj}', '${senha}');
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
